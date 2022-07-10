4af3ced6b0eac809206ea4e8f3e16ef803366ddf
Jkurbs <kerby.jean@hotmail.fr>
2022-07-10 19:35:22 -0400

    Added credentials view.

---
+++ Flaint/View/Auth/CredentialsView.swift
@@ -0,0 +1,21 @@
+//
+//  CredentialsView.swift
+//  Flaint
+//
+//  Created by Kerby Jean on 7/10/22.
+//  Copyright © 2022 Kerby Jean. All rights reserved.
+//
+
+import SwiftUI
+
+struct CredentialsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CredentialsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CredentialsView()
+    }
+}
